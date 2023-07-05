459cee7b500e2937ff17573d7def2ddf2cf9c864
Nurfaozi <nurfaozi1002@gmail.com>
2023-07-04 22:18:02 -0800

    final

--- application/controllers/backend/Dashboard.php
+++ application/controllers/backend/Dashboard.php
@@ -1,24 +1,14 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Dashboard extends MY_Controller {
 
-use Jenssegers\Blade\Blade;
-
-class Dashboard extends CI_Controller 
-{
 	public function index()
 	{
-		$this->load->view('template/backend/theme');
+		view('backend/dashboard/index');
 	}
-
-	public function test (){
-
-		$views = VIEWPATH;
-		$cache = APPPATH . 'cache';
-		$blade = new Blade($views, $cache);
-
-echo $blade->make('template/backend/tes', [])->render();
+	public function table()
+	{
+		view('backend/dashboard/table');
 	}
 }
-
